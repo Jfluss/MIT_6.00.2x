e9ff2c6d188cdccef2e88c944efa9951934a8c40
Jfluss <Jfluss@users.noreply.github.com>
2017-03-21 12:05:05 -0400

    exercise 1 solved

--- Lecture 2-1.py
+++ Lecture 2-1.py
@@ -10,10 +10,15 @@ Created on Mon Mar 20 10:11:21 2017
 def powerSet(items):
     N = len(items)
     # enumerate the 2**N possible combinations
-    for i in range(2**N):
-        combo = []
+    for i in range(3**N):
+        bag1 = []
+        bag2 = []
+        combo = (bag1, bag2)
         for j in range(N):
             # test bit jth of integer i
-            if (i >> j) % 2 == 1:
-                combo.append(items[j])
-            yield combo
+#            print((i >> j) % 3)
+            if (i // 3**j) % 3 == 1:   
+                bag1.append(items[j])
+            if (i // 3**j) % 3 == 2:
+                bag2.append(items[j])
+        yield combo
