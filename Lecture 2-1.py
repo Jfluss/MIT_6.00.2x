48a8109dd4bf2c7c752e4f21262faa5218744e29
Jfluss <Jfluss@users.noreply.github.com>
2017-03-21 12:09:18 -0400

    doc string

--- Lecture 2-1.py
+++ Lecture 2-1.py
@@ -6,8 +6,15 @@ Created on Mon Mar 20 10:11:21 2017
 @author: joshfluss
 """
 
-# generate all combinations of N items
-def powerSet(items):
+
+def yieldAllCombos(items):
+    """
+        Generates all combinations of N items into two bags, whereby each 
+        item is in one or zero bags.
+
+        Yields a tuple, (bag1, bag2), where each bag is represented as a list 
+        of which item(s) are in each bag.
+    """
     N = len(items)
     # enumerate the 2**N possible combinations
     for i in range(3**N):
@@ -16,7 +23,7 @@ def powerSet(items):
         combo = (bag1, bag2)
         for j in range(N):
             # test bit jth of integer i
-#            print((i >> j) % 3)
+            print(i, j)
             if (i // 3**j) % 3 == 1:   
                 bag1.append(items[j])
             if (i // 3**j) % 3 == 2:
